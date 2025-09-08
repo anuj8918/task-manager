2a4f26b807fa2ead65db35683e4bf5b493cb88b5
anuj8918 <anujm8918@gmail.com>
2025-09-08 12:49:22 +0530

    backend url added

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://task-manager-4q5x.onrender.com/api' });
 
 // Before request add token in headers
 API.interceptors.request.use((req) => {
