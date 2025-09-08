02c04d0ae86bc039e78e43a2e82d06d3974f3856
anuj8918 <anujm8918@gmail.com>
2025-09-08 12:42:09 +0530

    code ready for deployment

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            tls: true,   // ensure TLS
+            tls: true, 
         });
         console.log('MongoDB Connected...');
     } catch (err) {
